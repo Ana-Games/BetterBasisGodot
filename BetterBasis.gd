331f08d64b72fae5a033ec5d85c579204045c28e
Ana-Games <anagamedev@gmail.com>
2024-01-16 18:39:34 +0000

    Works with non-orthogonal basis

--- BetterBasis.gd
+++ BetterBasis.gd
@@ -3,85 +3,48 @@ class_name BetterBasis
 
 static func set_up(original: Basis, new_up: Vector3) -> Basis:
 	new_up = new_up.normalized()
-	var scale = Vector3(original.x.length(), original.y.length(), original.z.length())
-	var old_basis = original.orthonormalized() as Basis
+	var original_y = original.y.normalized()
 	
-	if new_up == old_basis.y: return original
-	
-	var angle = old_basis.y.angle_to(new_up)
-	var new_basis : Basis
-	new_basis.y = new_up
-	if old_basis.y == -new_up:
-		new_basis.x = old_basis.z
-		new_basis.z = old_basis.x
+	if new_up == original_y: return original
+	elif original_y == -new_up:
+		return Basis(original.z, new_up * original.y.length(), original.x)
 	else:
-		var cross = old_basis.y.cross(new_up).normalized()
+		var cross = original_y.cross(new_up).normalized()
+		var angle = original_y.angle_to(new_up)
 		if cross == Vector3.ZERO:
 			print("error setting up basis - cross product is 0")
 			return original
-		new_basis.z = old_basis.z.rotated(cross, angle)
-		new_basis.x = old_basis.x.rotated(cross, angle)
-	
-	new_basis = new_basis.orthonormalized()
-	new_basis.x *= scale.x
-	new_basis.y *= scale.y
-	new_basis.z *= scale.z
-	return new_basis
+		return original.rotated(cross,angle)
 
 static func set_right(original: Basis, new_right: Vector3) -> Basis:
 	new_right = new_right.normalized()
-	var scale = Vector3(original.x.length(), original.y.length(), original.z.length())
-	var old_basis = original.orthonormalized() as Basis
+	var original_x = original.x.normalized()
 	
-	if new_right == old_basis.x: return original
-	
-	var angle = old_basis.x.angle_to(new_right)
-	var new_basis : Basis
-	new_basis.x = new_right
-	
-	if old_basis.x == -new_right:
-		new_basis.y = old_basis.z
-		new_basis.z = old_basis.y
+	if new_right == original_x: return original
+	elif original_x == -new_right:
+		return Basis(new_right * original.x.length(), original.z, original.y)
 	else:
-		var cross = new_right.cross(old_basis.x).normalized()
+		var cross = original_x.cross(new_right).normalized()
+		var angle = original_x.angle_to(new_right)
 		if cross == Vector3.ZERO:
 			print("error setting right basis - cross product is 0")
 			return original
-		new_basis.y = old_basis.y.rotated(cross, angle)
-		new_basis.z = old_basis.z.rotated(cross, angle)
-	
-	new_basis = new_basis.orthonormalized()
-	new_basis.x *= scale.x
-	new_basis.y *= scale.y
-	new_basis.z *= scale.z
-	return new_basis
+		return original.rotated(cross,angle)
 
 static func set_forward(original: Basis, new_forward: Vector3) -> Basis:
 	new_forward = new_forward.normalized()
-	var scale = Vector3(original.x.length(), original.y.length(), original.z.length())
-	var old_basis = original.orthonormalized() as Basis
+	var original_z = original.z.normalized()
 	
-	if new_forward == old_basis.z: return original
-	
-	var angle = old_basis.z.angle_to(new_forward)
-	var new_basis : Basis
-	new_basis.z = new_forward
-	if old_basis.z == -new_forward:
-		new_basis.x = old_basis.y
-		new_basis.y = old_basis.x
+	if new_forward == original_z: return original
+	elif original_z == -new_forward:
+		return Basis(original.y,original.x, new_forward * original.z.length())
 	else:
-		var cross = old_basis.z.cross(new_forward).normalized()
+		var cross = original_z.cross(new_forward).normalized()
+		var angle = original_z.angle_to(new_forward)
 		if cross == Vector3.ZERO:
 			print("error setting up basis - cross product is 0")
 			return original
-		new_basis.y = old_basis.y.rotated(cross, angle)
-		new_basis.x = old_basis.x.rotated(cross, angle)
-	
-	new_basis = new_basis.orthonormalized()
-	new_basis.x *= scale.x
-	new_basis.y *= scale.y
-	new_basis.z *= scale.z
-	return new_basis
+		return original.rotated(cross, angle)
 
 static func get_up(basis : Basis) -> Vector3:
 	return basis.y.normalized()
